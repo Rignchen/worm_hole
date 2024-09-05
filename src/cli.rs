54dbb6d4bfba12b76e27387c3054d3a8161130d5
Rignchen <rinchendemort@gmail.com>
2024-09-05 08:12:46 +0200

    feat(cli): chose the cli structure

--- src/cli.rs
+++ src/cli.rs
@@ -4,6 +4,42 @@ structstruck::strike! {
     #[strikethrough[derive(Parser, Debug)]]
     #[clap(verbatim_doc_comment)]
     #[command(version)]
-    pub struct Args {}
+    pub struct Args {
+        #[clap(subcommand)]
+        pub cmd: pub enum Command {
+            #[clap(name = "cd")]
+            ChangeDir(
+                struct ChangeDir {
+                    pub alias: String,
+                    #[clap(short, long)]
+                    pub query: bool,
+                }
+            ),
+            #[clap(name = "add")]
+            AddAlias(
+                struct AddAlias {
+                    pub path: String,
+                    pub alias: String,
+                }
+            ),
+            #[clap(name = "rm")]
+            RemoveAlias(
+                struct RemoveAlias {
+                    pub alias: String,
+                }
+            ),
+            #[clap(name = "ls")]
+            ListAliases(
+                struct ListAliases {}
+            ),
+            #[clap(name = "edit")]
+            EditAlias(
+                struct EditAlias {
+                    pub alias: String,
+                    pub path: String,
+                }
+            ),
+        }
+    }
 }
 
