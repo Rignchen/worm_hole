f9c2f9a62abda26180893e652c3257ae7e3b09c5
Rignchen <rinchendemort@gmail.com>
2024-10-24 11:56:49 +0200

    feat: initialise table in the db

--- src/db.rs
+++ src/db.rs
@@ -9,8 +9,25 @@ impl Database {
     pub fn new(path: &str) -> WHResult<Database> {
         let connection = Connection::open(path);
         match connection {
-            Ok(connection) => Ok(Database { connection }),
+            Ok(connection) => {
+                let db = Database { connection };
+                db.init();
+                Ok(db)
+            }
             Err(_) => Err(WHError::DatabaseConnectionError(path.to_string())),
         }
+    }
+
+    pub fn init(&self) {
+        self.connection
+            .execute(
+                "create table if not exists aliases (
+                    id integer primary key,
+                    alias text not null,
+                    path text not null,
+                    unique(alias)
+                )",
+            )
+            .unwrap();
     }
 }
