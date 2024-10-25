3774c1ef1455d66277d689ed5431a0b9dae6d478
Rignchen <rinchendemort@gmail.com>
2024-10-25 16:11:55 +0200

    feat: allow user to chose aliases

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,15 +1,23 @@
 use crate::{db::Database, error::WHResult};
 use clap::Parser;
+use string_builder::Builder as StringBuilder;
 
 #[derive(Parser, Debug)]
 pub struct Init {
     /// The shell to generate the init code for
     shell: Shell,
+    // These flags will be used to set the commands to write in the console to call worm_hole
+    /// The command to use to call worm_hole
+    #[clap(long, default_value = "wh")]
+    worm_hole: String,
+    /// The command to use to change directory with worm_hole
+    #[clap(long, default_value = "whcd")]
+    cd: String,
 }
 impl Init {
     pub fn run(&self, database: &Database) -> WHResult<()> {
         database.init();
-        println!("{}", self.shell.get_init_code());
+        println!("{}", self.shell.get_init_code(&self));
         Ok(())
     }
 }
@@ -21,11 +29,18 @@ enum Shell {
     Zsh,
 }
 impl Shell {
-    pub fn get_init_code(&self) -> &'static str {
+    pub fn get_init_code(&self, aliases: &Init) -> String {
+        let mut builder = StringBuilder::default();
+        builder.append(format!("alias {}=worm_hole\n", aliases.worm_hole));
+        builder.append(self.get_cd_function());
+        builder.append(format!("alias {}=__worm_hole_cd\n", aliases.cd));
+        builder.string().unwrap()
+    }
+
+    fn get_cd_function(&self) -> &'static str {
         match self {
             Shell::Bash | Shell::Zsh => {
-                "alias 'wh=worm_hole'
-                __worm_hole_cd() {
+                "__worm_hole_cd() {
                     if [ -z \"$1\" ]
                     then
                         cd $HOME
@@ -33,11 +48,10 @@ impl Shell {
                         CD=$(wh query $1) && \\builtin cd $CD
                     fi
                 }
-                alias 'whcd=__worm_hole_cd'"
+                "
             }
             Shell::Fish => {
-                "alias 'wh=worm_hole'
-                function __worm_hole_cd
+                "function __worm_hole_cd
                     if test -z $argv
                         cd $HOME
                     else
@@ -47,7 +61,7 @@ impl Shell {
                         end
                     end
                 end
-                alias 'whcd=__worm_hole_cd'"
+                "
             }
         }
     }
