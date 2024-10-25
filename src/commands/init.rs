a59ab6025b4c662d20fa5edb8b219b6e8f6c4f60
Rignchen <rinchendemort@gmail.com>
2024-10-25 16:13:02 +0200

    feat: add multiple facultative custom aliases

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -6,13 +6,29 @@ use string_builder::Builder as StringBuilder;
 pub struct Init {
     /// The shell to generate the init code for
     shell: Shell,
-    // These flags will be used to set the commands to write in the console to call worm_hole
+    // All these flags will be used to set the commands to write in the console to call worm_hole
+    // Only the worm_hole and cd command has a default value
     /// The command to use to call worm_hole
     #[clap(long, default_value = "wh")]
     worm_hole: String,
     /// The command to use to change directory with worm_hole
     #[clap(long, default_value = "whcd")]
     cd: String,
+    /// The command to use to add a new path to worm_hole
+    #[clap(long)]
+    add: Option<String>,
+    /// The command to use to remove a path from worm_hole
+    #[clap(long)]
+    remove: Option<String>,
+    /// The command to use to list all the paths in worm_hole
+    #[clap(long)]
+    list: Option<String>,
+    /// The command to use to query a path in worm_hole
+    #[clap(long)]
+    query: Option<String>,
+    /// The command to use to edit a path in worm_hole
+    #[clap(long)]
+    edit: Option<String>,
 }
 impl Init {
     pub fn run(&self, database: &Database) -> WHResult<()> {
@@ -34,6 +50,21 @@ impl Shell {
         builder.append(format!("alias {}=worm_hole\n", aliases.worm_hole));
         builder.append(self.get_cd_function());
         builder.append(format!("alias {}=__worm_hole_cd\n", aliases.cd));
+        if let Some(add) = &aliases.add {
+            builder.append(format!("alias {}='worm_hole add'\n", add));
+        }
+        if let Some(remove) = &aliases.remove {
+            builder.append(format!("alias {}='worm_hole remove'\n", remove));
+        }
+        if let Some(list) = &aliases.list {
+            builder.append(format!("alias {}='worm_hole list'\n", list));
+        }
+        if let Some(query) = &aliases.query {
+            builder.append(format!("alias {}='worm_hole query'\n", query));
+        }
+        if let Some(edit) = &aliases.edit {
+            builder.append(format!("alias {}='worm_hole edit'\n", edit));
+        }
         builder.string().unwrap()
     }
 
